2e8b11cc19b1834b28c4c41a9468664beb80e632
hemanthkumar2002-cloud <sreenivasuluhemanth@gmail.com>
2024-09-16 10:54:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let todo=document.getElementById('todo');
+let empl=document.getElementById('employee');
+
+todo.addEventListener('click',function(){
+    window.location.href = "https://github.com/hemanthkumar2002-cloud/TO-DO-List";
+});
+empl.addEventListener('click',function(){
+    window.location.href="https://github.com/hemanthkumar2002-cloud/Employee-Management-System";
+})
